bbed04c3d11b0c2be06333e2b6cd15c40969f52a
Suxrobjonb <suxrobjonb6@gmail.com>
2022-09-13 21:45:37 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let elBody = document.querySelector("body");
+let elButton = document.querySelector(".site__mode");
+let elList = document.querySelector(".site__ul");
+let elInput = document.querySelector(".input");
+
+
+elButton.addEventListener("click", () => {
+    elBody.classList.toggle("dark__mode")   
+})
+
+elList = document.createElement("li")
+
+
+
+
